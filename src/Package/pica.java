49c9f9c346c0b0619565765230adb2a17c8bee4f
Kristians <kristianspavlovskis@gmail.com>
2023-03-23 17:29:09 +0200

    Uzstaisīta jauna klase

--- src/Package/pica.java
+++ src/Package/pica.java
@@ -1,18 +1,8 @@
 package Package;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.File;
-import java.util.regex.Pattern;
-
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JCheckBox;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 
 
-public class pica extends JFrame {
+
+public class pica {
 
 	public static void main(String[] args) {
 		logi checkbock = new logi();
